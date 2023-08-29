bac1762dd2725d8e5451eedd5c4cfe9a6d3d123d
RavshanbekIsroilov <isroilovravshan42@gmail.com>
2023-08-29 22:20:18 +0500

    first commit

---
+++ TelegramUI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TelegramUI.Models;
+
+namespace TelegramUI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TelegramUI.Models.User>? User { get; set; }
+    }
+}
---
+++ TelegramUI/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TelegramUI.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string UserName { get; set; }
+    public string Bio { get; set; }
+}
